bb2d3f536b46a804a75ba076f39dd25f23ac1e6a
Anwesh43 <anweshthecool0@gmail.com>
2017-03-30 21:17:16 +0530

    Adding ScrollListener

---
+++ leancollapsiblebar/src/main/java/com/anwesome/ui/leancollapsiblebar/ScrollListener.java
@@ -0,0 +1,8 @@
+package com.anwesome.ui.leancollapsiblebar;
+
+/**
+ * Created by anweshmishra on 30/03/17.
+ */
+public interface ScrollListener {
+    void handleScroll(float h,float gap,int dir);
+}
